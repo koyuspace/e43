694eb3bb1f8479067d366a2d2f5474d2b1f1bf2b
koyu <me@koyu.space>
2020-04-20 17:52:04 +0200

    Fix youtube-dl

--- main.py
+++ main.py
@@ -467,7 +467,7 @@ def handle(bot):
                                         done = True
                                     else:
                                         status_message = bot.sendMessage(chat_id, "Downloading...", reply_to_message_id=update.effective_message.message_id)
-                                        cmd_download = ["youtube-dl", "--no-continue", "-f", "mp4", "-o", "video.%(ext)s", input_text]
+                                        cmd_download = ["youtube-dl", "--no-continue", "-f", "mp4", "-o", "temp/video.%(ext)s", input_text]
                                         subprocess.Popen(cmd_download, shell=False).wait()
                                         cmd_conv = "ffmpeg -y -i temp/video.mp4 -vcodec libx264 -crf 27 -preset veryfast -c:a copy -s 640x360 out.mp4"
                                         if not chat_type == "channel" and not "group" in chat_type:
@@ -673,7 +673,7 @@ def handle(bot):
                                             username = line.split(":")[1]
                                             username = "\n🆔 @" + username
                                     if "mixcloud" in input_text:
-                                        cmd = ["youtube-dl", "--add-metadata", "-x", "--no-continue", "--prefer-ffmpeg", "--extract-audio", "--write-thumbnail", "--embed-thumbnail", "-v", "--audio-format", "mp3", "--output", "audio.%%(ext)s", input_text]
+                                        cmd = ["youtube-dl", "--add-metadata", "-x", "--no-continue", "--prefer-ffmpeg", "--extract-audio", "--write-thumbnail", "--embed-thumbnail", "-v", "--audio-format", "mp3", "--output", "temp/audio.%%(ext)s", input_text]
                                         subprocess.Popen(cmd, shell=False).wait()
                                         r = requests.get(input_text)
                                         c = r.content
@@ -731,7 +731,7 @@ def handle(bot):
                                         os.system("wget -O temp/audio.jpg \"" + cover + "\"")
                                         query = artist.replace("(", " ").replace(")", "").lower() + " " + title.replace("(", " ").replace(")", "").lower().replace(" ", "+")
                                         print(query)
-                                        cmd = ["youtube-dl", "--no-continue", "--add-metadata", "-x", "--prefer-ffmpeg", "--extract-audio", "-v", "--audio-format", "mp3", "--output", "audio.%%(ext)\"", "ytsearch:\"" + query + "\""]
+                                        cmd = ["youtube-dl", "--no-continue", "--add-metadata", "-x", "--prefer-ffmpeg", "--extract-audio", "-v", "--audio-format", "mp3", "--output", "temp/audio.%%(ext)\"", "ytsearch:\"" + query + "\""]
                                         subprocess.Popen(cmd, shell=False).wait()
                                         filename = artist.replace(" ", "-").replace("/", "-") + "_" + title.replace(" ", "-").replace("/", "-") + ".mp3"
                                         if not chat_type == "channel" and not "group" in chat_type:
@@ -833,7 +833,7 @@ def handle(bot):
                                             bot.sendMessage(chat_id,"Here you go!\nCheck out @kseverythingbot_army for news and informations about this bot.",disable_web_page_preview=True)
                                     if "youtu" in input_text:
                                         input_text = input_text.replace("music.", "")
-                                        cmd = ["youtube-dl", "--add-metadata", "-x", "--no-continue", "--prefer-ffmpeg", "--extract-audio", "--write-thumbnail", "--embed-thumbnail", "-v", "--audio-format", "mp3", "--output", "audio.%%(ext)s", input_text]
+                                        cmd = ["youtube-dl", "--add-metadata", "-x", "--no-continue", "--prefer-ffmpeg", "--extract-audio", "--write-thumbnail", "--embed-thumbnail", "-v", "--audio-format", "mp3", "--output", "temp/audio.%%(ext)s", input_text]
                                         subprocess.Popen(cmd, shell=False).wait()
                                         tag = eyed3.load("temp/audio.ogg")
                                         try:
